97931e2109465ab9851cc5cfdc647aab4c146b8f
Ginara Sorin <49222989+ginaras@users.noreply.github.com>
2024-04-04 13:05:27 +0300

    creare  bd si generare cheie

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module org.softconsult.keydelicenta {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
+    requires java.sql;
 
     opens org.softconsult.keydelicenta to javafx.fxml;
     exports org.softconsult.keydelicenta;
--- src/main/java/org/softconsult/keydelicenta/ControllerStage0.java
+++ src/main/java/org/softconsult/keydelicenta/ControllerStage0.java
@@ -5,7 +5,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Random;
 import java.util.ResourceBundle;
 
@@ -23,9 +29,41 @@ public class ControllerStage0 implements Initializable {
     public RadioButton radioButtonLocal;
     public ComboBox comboBoxMonth;
     public ComboBox comboBoxYear;
+    static String generatedDB;
+    public TextField textFieldServer;
+    String generatedUSerName;
+    String generatedPass;
+
+    public ControllerStage0() throws SQLException {
+    }
+
+    public static String bazaDeDate = "information_schema";
+    public static String backUpbazaDeDate = "autoNelYFunDa250223Bbackup";
+    public static String USER=  "aUtONeli";
+    public static String PASSWORD=  "pas@NeLy2gynLog23";
+    //    public static String SERVER="95.214.135.69";
+//        public static String SERVER="localhost";
+//        public static String SERVER="10.59.4.8";
+    String SERVER;
+    public static String PORT = "3306";
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        textFieldServer.setVisible(false);
+        comboBoxApp.setItems(FXCollections.observableArrayList("Auto", "Managementul Proiectelor"));
+        comboBoxMonth.setItems(FXCollections.observableArrayList("ian", "feb", "mar", "apr", "mai", "iun", "iul", "aug", "sep", "oct", "noi", "dec"));
+        comboBoxYear.setItems(FXCollections.observableArrayList("24", "25", "26", "27", "28", "29", "30"));
+
+        ToggleGroup toggleGroup=new ToggleGroup();
+        radioButtonNet.setToggleGroup(toggleGroup);
+        radioButtonLocal.setToggleGroup(toggleGroup);
+        radioButtonServerPropriu.setToggleGroup(toggleGroup);
+        radioButtonNet.setSelected(true);
 
 
-    public void buttonGenerateAct(ActionEvent actionEvent) {
+    }
+
+    public void buttonGenerateAct(ActionEvent actionEvent) throws SQLException, IOException {
         if(comboBoxApp.getValue()==null){
             Alert alert=new Alert(Alert.AlertType.ERROR,"Nu ai ales aplicatia");
             alert.showAndWait();
@@ -36,32 +74,40 @@ public class ControllerStage0 implements Initializable {
             alert.showAndWait();
             return;
         }
+        if(radioButtonServerPropriu.isSelected()){
+        if(textFieldServer.getText().isEmpty()){
+            Alert alert=new Alert(Alert.AlertType.ERROR,"Serverul de intranet");
+            alert.showAndWait();
+            return;
+        }}
         if(comboBoxMonth.getValue()==null){
             Alert alert=new Alert(Alert.AlertType.ERROR,"Nu ai ales luna");
             alert.showAndWait();
             return;
         }
-        if(comboBoxYear.getValue()==null){
-            Alert alert=new Alert(Alert.AlertType.ERROR,"Nu ai ales anul");
-            alert.showAndWait();
-            return;
-        }
+
+            if(comboBoxYear.getValue()==null ){
+                Alert alert=new Alert(Alert.AlertType.ERROR,"Nu ai ales anul");
+                alert.showAndWait();
+                return;
+            }
+
         int leftLimit = 48; // numeral '0'
         int rightLimit = 122; // letter 'z'
         int targetStringLength = 10;
         Random random = new Random();
 
-        String generatedDB = random.ints(leftLimit, rightLimit + 1)
+        generatedDB = random.ints(leftLimit, rightLimit + 1)
                 .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
                 .limit(targetStringLength)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
-        String generatedUSerName = random.ints(leftLimit, rightLimit + 1)
+        generatedUSerName = random.ints(leftLimit, rightLimit + 1)
                 .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
                 .limit(targetStringLength)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
-        String generatedPass = random.ints(leftLimit, rightLimit + 1)
+        generatedPass = random.ints(leftLimit, rightLimit + 1)
                 .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
                 .limit(targetStringLength)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
@@ -80,27 +126,94 @@ public class ControllerStage0 implements Initializable {
         String inverseMonthStr=monthStr.substring(2,3)+monthStr.substring(1,2)+monthStr.substring(0,1);
         String inverseYearhStr=yearStr.substring(1,2)+yearStr.substring(0,1);
         String server=null;
-        System.out.println("autoZ80uhWQ9MlJAyfaOKyHApHx13xl70Z52ram".length());
 
+        if(radioButtonNet.isSelected()) { server="n";  SERVER= "95.214.135.69";     }
+        if(radioButtonLocal.isSelected()){server="l"; SERVER="localhost";}
+        if(radioButtonServerPropriu.isSelected()){server="s"; SERVER=textFieldServer.getText();}
 
-        if(radioButtonNet.isSelected()) { server="n";        }
-        if(radioButtonLocal.isSelected()){server="l";}
-        if(radioButtonServerPropriu.isSelected()){server="s";}
-
+        System.out.println(SERVER);
         labelKeie.setText(app+textFieldDB.getText()+textFieldUserName.getText()+textFieldPass.getText()+inverseYearhStr+inverseMonthStr+server);
         textFieldKeie.setText(app+textFieldDB.getText()+textFieldUserName.getText()+textFieldPass.getText()+inverseYearhStr+inverseMonthStr+server);
+
+        FileWriter fileWriter=new FileWriter("credentiale/"+comboBoxApp.getValue().toString()+"-"+textFieldClient.getText());
+        fileWriter.append(textFieldClient.getText()+"\n");
+        fileWriter.append("DB-"+textFieldDB.getText()+"\n");
+        fileWriter.append("UN-"+textFieldUserName.getText()+"\n");
+        fileWriter.append("P-"+textFieldPass.getText()+"\n");
+        if(radioButtonNet.isSelected()){ fileWriter.append("pe net+\n");}
+        if(radioButtonLocal.isSelected()){ fileWriter.append("local+\n");}
+        if(radioButtonServerPropriu.isSelected()){ fileWriter.append("srv-"+textFieldServer.getText()+"\n");}
+        fileWriter.append("   -----------------"+"\n");
+        fileWriter.append("Licenta: "+labelKeie.getText()+"\n");
+        fileWriter.close();
+
+        System.out.println(labelKeie.getText().length());
+        String CREATEUSERNAME = "CREATE USER IF NOT EXISTS "+USER+"@"+SERVER+" identified by "+PASSWORD+" ";
+        String URL = String.format( "jdbc:mariadb://"+SERVER+":"+PORT+"/"+bazaDeDate+"" );
+//        public static String URL = String.format( "jdbc:mariadb://95.214.135.69:3306/autoNelYFunDa250223" );
+//    public static String URLbackup = String.format( "jdbc:mariadb://'"+SERVER+"':'"+PORT+"'/'"+backUpbazaDeDate+"'" );
+
+          String URLbackup = String.format( "jdbc:mariadb://"+SERVER+":"+PORT+"/"+backUpbazaDeDate+"" );
+         String USE_DATABASE = "USE "+generatedDB+" ";
+         String USE_DATABASE_backup = "USE '"+backUpbazaDeDate+"'";
+         String selectUser ="SELECT USER '"+USER+"' FROM mysql.user";
+try {
+    Connection connectionFirst = DriverManager.getConnection(URL, USER, PASSWORD);
+    Statement statementINV = connectionFirst.createStatement();
+        statementINV.executeQuery("CREATE DATABASE IF NOT EXISTS "+textFieldDB.getText()+"");
+        statementINV.executeUpdate(USE_DATABASE);
+        statementINV.executeUpdate(CREATE_AUTO_TBL);
+        statementINV.executeUpdate(CREATE_RCA_ITP_AURORIZARI);
+        statementINV.executeUpdate(CREATE_LISTA_DOCUMENTE);
+        statementINV.executeUpdate(CREATE_TABLE_FACTURI_CARBURANTI);
+        statementINV.executeUpdate(CREATE_CONSUM_CARBURANTI);
+        statementINV.executeUpdate(CREATE_PIESE);
+        statementINV.executeUpdate(CREATE_TABLE_LOG);
+        statementINV.executeUpdate(CREATE_TABLE_AUTORIZARI_AUTO);
+        statementINV.executeUpdate(CREATE_TABLE_RESP_PRIVILEGE);
+        statementINV.executeUpdate(CREATE_TABLE_REVIZII);
+        statementINV.executeUpdate(CREATE_TABLE_ADMINSTRATIV);
+} catch(SQLException e) {
+    Alert alert = new Alert(Alert.AlertType.INFORMATION, "nu s-a putut conecta la baza de date");
+    alert.showAndWait();
+}
+
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, "ai creat tabelele");
+        alert.setHeaderText("AI CREAT TABLELE");
+        alert.showAndWait();
+
+        buttonGenerate.setDisable(true);
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        comboBoxApp.setItems(FXCollections.observableArrayList("Auto", "Managementul Proiectelor"));
-        comboBoxMonth.setItems(FXCollections.observableArrayList("ian", "feb", "mar", "apr", "mai", "iun", "iul", "aug", "sep", "oct", "noi", "dec"));
-        comboBoxYear.setItems(FXCollections.observableArrayList("24", "25", "26", "27", "28", "29", "30"));
 
-        ToggleGroup toggleGroup=new ToggleGroup();
-        radioButtonNet.setToggleGroup(toggleGroup);
-        radioButtonLocal.setToggleGroup(toggleGroup);
-        radioButtonServerPropriu.setToggleGroup(toggleGroup);
-        radioButtonNet.setSelected(true);
+
+//    public Void Auto {
+
+
+
+
+
+
+
+//        public static final String CREATE_DATABASE="CREATE DATABASE IF NOT EXISTS '"+textF+"'";
+        public static final String CREATE_AUTO_TBL= "CREATE TABLE IF NOT EXISTS auto (nrCrt INT(4) AUTO_INCREMENT PRIMARY KEY, nrInventar VARCHAR (15), nrInmatriculare VARCHAR (12), marca VARCHAR (40), model VARCHAR (40), tip VARCHAR (50), combustibil VARCHAR (20), capacitateRezervor INT(4), anvelopeMarca VARCHAR (30), anvelopeTip VARCHAR (30), serieSasiu VARCHAR (20), valAchizitie VARCHAR (12) , dataAchizitiei DATE, dataPrimeiInmatr DATE, nrAxe INT(2), kmInitiali INT(7), lungime VARCHAR(6), latime VARCHAR(6), inaltime VARCHAR(4), gabarit VARCHAR(6), operare VARCHAR (100)  )";
+        public static final String CREATE_RCA_ITP_AURORIZARI= "CREATE TABLE IF NOT EXISTS rcaITP (nrCrt INT(5) AUTO_INCREMENT PRIMARY KEY, document VARCHAR (20), nrInmatriculare VARCHAR (12), tipDocument VARCHAR(20), nrDoc VARCHAR (20), dataDoc DATE, dataExpirare DATE, durataRamasaTemp INT (4), valoare DOUBLE, inlocuita INT (4), operare VARCHAR (100) )";
+        public static final String CREATE_LISTA_DOCUMENTE= "CREATE TABLE IF NOT EXISTS listaDocumente (nrCrtLista INT(2), denDoc VARCHAR (50), inactiv VARCHAR (2) )";
+        public static final String CREATE_TABLE_FACTURI_CARBURANTI="CREATE TABLE IF NOT EXISTS factCarburant ( nrCrt INT (6) AUTO_INCREMENT PRIMARY KEY, furnizor VARCHAR(70), nrFactura VARCHAR(10), dataFactura DATE, tipCombustibil VARCHAR(12), cantitate VARCHAR(7), pretUnitar VARCHAR(15), operare VARCHAR (100), nrInmatriculare VARCHAR(12) )";
+        public static final String CREATE_CONSUM_CARBURANTI= "CREATE TABLE IF NOT EXISTS consumCarburant (nrCrtCombust INT(6) AUTO_INCREMENT PRIMARY KEY, nrInmatriculare VARCHAR (12), tipCombustibil VARCHAR(20), cantitate  VARCHAR (6), kmLaAlimentare VARCHAR (7), consum DOUBLE, dataAlimentarii DATE, operare VARCHAR (100)  )";
+        public static final String CREATE_PIESE= "CREATE TABLE IF NOT EXISTS piese (nrCrtPiese INT(5) AUTO_INCREMENT PRIMARY KEY, furnizor VARCHAR (50), nrFactura VARCHAR (20), dataFactura DATE, piesa VARCHAR(50), pret VARCHAR (10), bonConsum VARCHAR(20), dataBonConsum DATE, nrInmatriculare VARCHAR (12), operare VARCHAR (100) )";
+        public static final String CREATE_TABLE_RESP_PRIVILEGE = "CREATE TABLE IF NOT EXISTS respPrivilege (nrCrt INT (3) AUTO_INCREMENT PRIMARY KEY, respProiect VARCHAR(50), macNr VARCHAR(17), adminu CHAR(10), sex CHAR (1), inactiv VARCHAR (3), rezerva1 VARCHAR (50))";
+        public static final String CREATE_TABLE_AUTORIZARI_AUTO = "CREATE TABLE IF NOT EXISTS autorizariAuto ( nrCrt INT(4) AUTO_INCREMENT PRIMARY KEY, nrInmatriculare VARCHAR (12), tipAutorizatie VARCHAR (30), nrDocumentAutorizare VARCHAR (20), dataAutorizarii DATE, dataExpirariiAutorizarii DATE, valoare VARCHAR (12), operare VARCHAR (100))";
+        public static final String CREATE_TABLE_REVIZII="CREATE TABLE IF NOT EXISTS revizii (nrCrt INT(5) AUTO_INCREMENT PRIMARY KEY, nrAuto VARCHAR(12), document VARCHAR (100), dataReviziei DATE, dataExpirarii DATE, durataRamasaTemp INT(4), kmLaRevizie DOUBLE, kmLaUrmRevizie DOUBLE, valoare DOUBLE,  inlocuita INT(4), operare VARCHAR (100) )";
+        public static final String CREATE_TABLE_LOG = "CREATE TABLE IF NOT EXISTS logFile (dataLogarii VARCHAR (60), utilizator VARCHAR(50), mesaj VARCHAR (1000), rezervaLog VARCHAR(70))";
+        public static final String CREATE_TABLE_ADMINSTRATIV="CREATE TABLE IF NOT EXISTS administrativ (nrCrt INT(4) AUTO_INCREMENT PRIMARY KEY, detaliu VARCHAR(100), data DATE, parametru VARCHAR(20), descriere VARCHAR(200))";
+
+
+    public void radioButtonServerPropriuAct(ActionEvent actionEvent) {
+        textFieldServer.setVisible(true);
     }
+
+//    }
+
 }
